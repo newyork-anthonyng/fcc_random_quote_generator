122449d7608c862a0412e836c5ec20e54361e4a8
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2016-01-15 16:45:37 -0500

    Add twitter button

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.get('/quote', (req, res) => {
   });
 });
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log('Express server running.');
 });
